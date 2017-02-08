06e1e6faf1b553ae7d2499609b76471a41be2730
Steven Zafrani <zsteven44@gmail.com>
2017-02-08 09:12:55 -0800

    preference xml modified and options added

--- app/src/main/java/com/example/stevenzafrani/congregate/fragments/AlgorithmFragment.java
+++ app/src/main/java/com/example/stevenzafrani/congregate/fragments/AlgorithmFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.stevenzafrani.congregate.R;
@@ -28,8 +31,27 @@ public class AlgorithmFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        Spinner spinner = (Spinner) getActivity().findViewById(R.id.spinner_algorithm_sort);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         TextView textview;
         nodeList.clear();
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.algorithm_sort_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
         float randomVal;
 
         for (int i=0; i<20; i++) {
@@ -37,6 +59,7 @@ public class AlgorithmFragment extends Fragment {
             textview = new TextView(((getContext())));
             textview.setBackgroundColor(getResources().getColor(R.color.colorNode));
             textview.setWidth(45);
+            textview.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
             randomVal = ((float) Math.random()*100);
             textview.setTextSize(textSize);
             textview.setHeight(30+((int)randomVal));
@@ -47,4 +70,8 @@ public class AlgorithmFragment extends Fragment {
         }
 
     }
+
+
+
+
 }
