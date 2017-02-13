30bdf2bc9b9197d16274aecb4a86c2caef3bd6a8
Steven Zafrani <zsteven44@gmail.com>
2017-02-13 09:21:55 -0800

    incremental display of sorting mechanisms for bubble sort

--- app/src/main/java/com/example/stevenzafrani/congregate/algorithms/sort/BubbleSort.java
+++ app/src/main/java/com/example/stevenzafrani/congregate/algorithms/sort/BubbleSort.java
@@ -1,18 +1,22 @@
 package com.example.stevenzafrani.congregate.algorithms.sort;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.example.stevenzafrani.congregate.models.AlgorithmPass;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BubbleSort {
-
+    ArrayList<AlgorithmPass> algorithmLog = new ArrayList<>();
     public BubbleSort(final Context context,
                       final int array[],
                       final ImageView canvas){
         Log.v(BubbleSort.class.getSimpleName(), Arrays.toString(array));
-
+        algorithmLog.clear();
         int n = array.length;
         int k;
         for (int i = n; i >= 0; i--) {
@@ -20,15 +24,27 @@ public class BubbleSort {
                 k = j + 1;
                 if (array[j] > array[k]) {
                     swapNumbers(j, k, array);
+
+                    // Testing Log
+                    algorithmLog.add(new AlgorithmPass(i, array));
                 }
-
-
             }
             Log.v(BubbleSort.class.getSimpleName(), "Pass " + Integer.toString(n-i) + " " + Arrays.toString(array));
-
-
         }
         printResult(array);
+        for (int z =0; z<algorithmLog.size(); z++) {
+            final AlgorithmPass tempAlgorithmPass = new AlgorithmPass(algorithmLog.get(z).getPassNumber(),algorithmLog.get(z).getArrayValues());
+            Handler handler1 = new Handler();
+            handler1.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    runSimulation(tempAlgorithmPass, array);
+                }
+            }, 1000);
+
+        }
+
+
 
     }
 
@@ -39,16 +55,7 @@ public class BubbleSort {
         int temp = array[k];
         array[k] = array[j];
         array[j] = temp;
-        /*
-        Log.v(BubbleSort.class.getSimpleName(), "About to run handler with postDelayed.")
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
 
-            }
-        }, 100);
-        */
 
     }
 
@@ -56,4 +63,9 @@ public class BubbleSort {
         Log.v(BubbleSort.class.getSimpleName(), Arrays.toString(array));
     }
 
+    public void runSimulation(AlgorithmPass algorithmPass, int[] array) {
+
+        array = algorithmPass.getArrayValues();
+
+    }
 }
