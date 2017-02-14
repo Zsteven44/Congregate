3bd87103219e15920a803447ba29900ff68bf9fc
Steven Zafrani <zsteven44@gmail.com>
2017-02-14 15:32:59 -0800

    added algorithm, api activity

---
+++ app/src/main/java/com/example/stevenzafrani/congregate/activities/AlgorithmActivity.java
@@ -0,0 +1,33 @@
+package com.example.stevenzafrani.congregate.activities;
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AlgorithmActivity extends BaseActivity {
+
+    public AlgorithmActivity() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+}
