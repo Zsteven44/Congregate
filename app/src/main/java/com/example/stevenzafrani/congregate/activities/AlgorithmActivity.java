590e716e4c6ed7da4c2a749dc22d31579f7bcd1a
Steven Zafrani <zsteven44@gmail.com>
2017-02-14 15:40:49 -0800

    Fixing up algorithmActivity

--- app/src/main/java/com/example/stevenzafrani/congregate/activities/AlgorithmActivity.java
+++ app/src/main/java/com/example/stevenzafrani/congregate/activities/AlgorithmActivity.java
@@ -2,10 +2,21 @@ package com.example.stevenzafrani.congregate.activities;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.stevenzafrani.congregate.R;
+import com.example.stevenzafrani.congregate.fragments.AlgorithmFragment;
+
 public class AlgorithmActivity extends BaseActivity {
+    TabItem youtubeButton;
+    TabItem algorithmButton;
+    TabItem homeButton;
+    TabLayout homeTab;
+    ViewPager viewPager;
 
     public AlgorithmActivity() {
     }
@@ -13,6 +24,10 @@ public class AlgorithmActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_algorithm);
+        if (savedInstanceState ==null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new AlgorithmFragment()).commit();
+        }
     }
 
     @Override
