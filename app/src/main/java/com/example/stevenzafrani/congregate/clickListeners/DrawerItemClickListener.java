57b8632c2037949ce2332e2b448eeec1c3e56bee
Steven Zafrani <zsteven44@gmail.com>
2017-02-15 15:11:00 -0800

    activity intent for navigation drawer

--- app/src/main/java/com/example/stevenzafrani/congregate/clickListeners/DrawerItemClickListener.java
+++ app/src/main/java/com/example/stevenzafrani/congregate/clickListeners/DrawerItemClickListener.java
@@ -1,14 +1,26 @@
 package com.example.stevenzafrani.congregate.clickListeners;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.widget.AdapterView;
 
 public class DrawerItemClickListener implements AdapterView.OnItemClickListener {
+    Activity activity;
+    public DrawerItemClickListener(Context context) {
+        this.activity = (Activity) context;
+    }
+
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        /**
-         * Start intent to new activity here.
-         */
+        switch (i) {
+
+        }
+
+        /*
+        Intent intent = new Intent();
+        activity.startActivity();
+        */
     }
 }
