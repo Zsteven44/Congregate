c80463ff4756c0e881e98d9faf06b84f0460cc68
Steven Zafrani <zsteven44@gmail.com>
2017-02-28 11:33:26 -0800

    search algo log and pass redefined

--- app/src/main/java/com/example/stevenzafrani/congregate/models/AlgorithmPassSearch.java
+++ app/src/main/java/com/example/stevenzafrani/congregate/models/AlgorithmPassSearch.java
@@ -2,5 +2,24 @@ package com.example.stevenzafrani.congregate.models;
 
 public class AlgorithmPassSearch {
     public int passNumber;
+    public int passIndex;
+    public int passValue;
 
+    public AlgorithmPassSearch(int passNumber, int index, int value) {
+        this.passNumber = passNumber;
+        this.passIndex = index;
+        this.passValue = value;
+    }
+
+    public int getPassNumber() {
+        return passNumber;
+    }
+
+    public int getPassIndex() {
+        return passIndex;
+    }
+
+    public int getPassValue() {
+        return passValue;
+    }
 }
