c52f0e17e85795894166e9c469d48115e1aa118d
Steven Zafrani <zsteven44@gmail.com>
2017-03-03 12:35:40 -0800

    binary search fixed to function correctly

--- app/src/main/java/com/example/stevenzafrani/congregate/algorithms/search/BinarySearch.java
+++ app/src/main/java/com/example/stevenzafrani/congregate/algorithms/search/BinarySearch.java
@@ -3,6 +3,7 @@ package com.example.stevenzafrani.congregate.algorithms.search;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.example.stevenzafrani.congregate.models.AlgorithmLogSearch;
 import com.example.stevenzafrani.congregate.models.AlgorithmPassSearch;
@@ -17,23 +18,26 @@ public class BinarySearch extends BaseSearch {
     }
 
     public int performSearch(int[] array, int key) {
-        int max = array.length+1;
+        int max = array.length-1;
         int min = 0;
         int guess;
-        int counter= 0;
 
         while (max >= min) {
+
             counter++;
-            guess = (int) Math.floor(min+max/2);
+            guess = (int) Math.floor((min+max)/2);
+            Log.v(BinarySearch.class.getSimpleName(), "Guessing: " + guess);
             if (array[guess] == key) {
                 AlgorithmPassSearch algorithmPassSearch = new AlgorithmPassSearch(counter, guess, array[guess], true, array);
                 algorithmLogSearch.add(algorithmPassSearch);
                 return guess;
-            } else if (array[guess] > key) {
+            } else if (array[guess] < key) {
                 min = guess+1;
             } else {
                 max = guess-1;
             }
+            AlgorithmPassSearch algorithmPassSearch = new AlgorithmPassSearch(counter, guess, array[guess], false, array);
+            algorithmLogSearch.add(algorithmPassSearch);
         }
         return -1;
     }
